6fd9224839522fca65a2d40613eeadeecaa37676
Alex Vladut <alexandruvladut@gmail.com>
2020-08-01 12:33:30 +0300

    chore(app): initialise project base structure

---
+++ apps/todo-api/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  name: 'todo-api',
+  preset: '../../jest.config.js',
+  globals: {
+    'ts-jest': {
+      tsConfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  coverageDirectory: '../../coverage/apps/todo-api',
+};
